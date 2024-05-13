271bc3d829c50e75adc3d9efcfddad27e40378bd
Heidy Nicole Arancibia <arancibiaheidy36@gmail.com>
2024-05-12 21:32:17 -0400

    net

---
+++ infejer4/infejer4/WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace infejer4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string nom= Request.Params["nom"].ToString();
+            string ap = Request.Params["ap"].ToString();
+            Response.Write("Nombre  " + nom);
+            Response.Write("Apellido   "  + ap);
+        }
+    }
+};
